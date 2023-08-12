1f473d1113ea146a25417d1f54491f05e19f9a35
Vaishnavisingh327 <134056889+Vaishnavisingh327@users.noreply.github.com>
2023-08-12 20:34:16 +0530

    Add files via upload

---
+++ admin/mysqli_connect.php
@@ -0,0 +1,8 @@
+<?php
+$host = "localhost:3306"; 
+$user = "id21136829_car";
+$password = "5Vaishnavi@";
+$database = "id21136829_carrental";
+
+$connection = mysqli_connect ($host, $user, $password) or die('Not connected :error' . mysqli_error());
+?>
